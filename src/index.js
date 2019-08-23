ccf43335472a9f8f07c5eeae9f826b903ba711cc
doubleface <christophe@cozycloud.cc>
2019-08-23 10:46:36 +0200

    feat: no more bank operation linking in the connector
    A bank service will handle this

--- src/index.js
+++ src/index.js
@@ -43,7 +43,8 @@ async function start(fields) {
     identifiers: ['alan'],
     keys: ['vendorRef', 'beneficiary', 'date'],
     sourceAccount: this.accountId,
-    sourceAccountIdentifier: fields.login
+    sourceAccountIdentifier: fields.login,
+    linkBankOperations: false
   })
 
   await saveFiles(

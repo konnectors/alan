badac2bcad17e28e622bb9418db46267e0ab92d5
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2022-01-12 09:52:25 +0100

    feat: all features implemented

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 process.env.SENTRY_DSN =
   process.env.SENTRY_DSN ||
-  'https://c3b054eb15454092a0b64d6ec6527d23@sentry.cozycloud.cc/119'
+  'https://5642dd9ef4654b778964df826b0f10f4@errors.cozycloud.cc/22'
 
 const {
   BaseKonnector,
   requestFactory,
   log,
-  errors
+  errors,
+  utils,
+  cozyClient
 } = require('cozy-konnector-libs')
 const jwt = require('jwt-decode')
 const moment = require('moment')
 const groupBy = require('lodash/groupBy')
+
+const models = cozyClient.new.models
+const { Qualification } = models.document
+
 const request = requestFactory({
   // debug: true,
   cheerio: false,
@@ -19,8 +25,7 @@ const request = requestFactory({
 })
 
 const baseUrl = 'https://alan.eu'
-const apiUrl = 'https://api.alan.eu'
-const set = require('lodash/set')
+const apiUrl = 'https://api.alan.com'
 
 module.exports = new BaseKonnector(start)
 
@@ -35,8 +40,9 @@ async function start(fields) {
   await this.saveBills(bills, fields.folderPath, {
     identifiers: ['alan'],
     keys: ['vendorRef', 'beneficiary', 'date'],
-    fileIdAttributes: ['filename'],
-    linkBankOperations: false
+    fileIdAttributes: ['vendorRef'],
+    linkBankOperations: false,
+    sourceAccountIdentifier: fields.login
   })
 
   await this.saveFiles(
@@ -62,7 +68,27 @@ async function start(fields) {
 
 async function fetchData(user) {
   const { beneficiaries, tp_card_identifier } = await request(
-    `${apiUrl}/api/users/${user.userId}?expand=beneficiaries.insurance_profile.legacy_coverages,beneficiaries.insurance_profile.settlements,beneficiaries.insurance_profile.teletransmission_status_to_display,beneficiaries.insurance_profile.user.current_settlement_iban,invoices,insurance_profile,address,current_billing_iban,current_settlement_iban,current_exemption.company.current_contract.current_prevoyance_contract.prevoyance_plan,company.current_contract.current_prevoyance_contract.prevoyance_plan,company.current_contract.current_plan,company.current_contract.discounts,insurance_profile.current_policy.contract.current_plan,insurance_profile.current_policy.contract.contractee,legacy_health_contract,current_contract.madelin_attestations,current_contract.amendments,current_contract.current_plan,accountant,insurance_documents,insurance_documents.quotes,authorized_billing_ibans`,
+    `${apiUrl}/api/users/${user.userId}?expand=address,admined_companies,beneficiaries.insurance_profile.legacy_coverages,beneficiaries.insurance_profile.teletransmission_status_to_display,beneficiaries.insurance_profile.user.current_settlement_iban,beneficiaries.insurance_profile.current_attestation,beneficiaries.insurance_profile.internalised_teletransmission_statuses_by_ssn,beneficiaries.insurance_profile.internalisation_switch_date,beneficiaries.insurance_profile.latest_tp_card,current_contract.amendments,current_contract.current_amendment,current_exemption,current_exemption.current_justification,current_plan,current_plan.price_rules,current_plan.health_coverage.rendered_guarantees_with_updated_emojis,insurance_profile.current_policy.contract,insurance_profile.current_policy.pec_requests,insurance_profile.current_policy.tp_card_coverages,insurance_profile.current_policy.option_contract,insurance_profile.latest_tp_card,insurance_profile,legacy_health_contract,visible_notifications,feedback`,
+    {
+      auth: {
+        bearer: user.token
+      }
+    }
+  )
+
+  const decomptes = await request(
+    `${apiUrl}/api/users/${user.userId}?expand=visible_insurance_documents`,
+    {
+      auth: {
+        bearer: user.token
+      }
+    }
+  )
+  const documents = decomptes.visible_insurance_documents
+  const beneficiariesIds = documents[0].beneficiaries_insurance_profile_ids[0]
+
+  const events = await request(
+    `${apiUrl}/api/insurance_profiles/${beneficiariesIds}/care_events_public`,
     {
       auth: {
         bearer: user.token
@@ -71,27 +97,40 @@ async function fetchData(user) {
   )
 
   let bills = []
+
   for (const beneficiary of beneficiaries) {
     const name = beneficiary.insurance_profile.user.normalized_full_name
+
     bills.push.apply(
       bills,
-      beneficiary.insurance_profile.settlements
-        .filter(bill => bill.reimbursement_status === 'processed')
+      events
+        .filter(bill => bill.status === 'refunded')
         .map(bill => ({
           vendor: 'alan',
-          vendorRef: bill.created_at,
+          vendorRef: bill.id,
           beneficiary: name,
           type: 'health_costs',
           date: moment(bill.estimated_payment_date, 'YYYY-MM-DD').toDate(),
           originalDate: moment(bill.care_date, 'YYYY-MM-DD').toDate(),
-          subtype: bill.displayed_label,
-          description: bill.care_type_desc,
-          socialSecurityRefund: bill.ss_amount / 100,
-          amount: bill.covered_amount / 100,
-          originalAmount: bill.total_amount / 100,
-          isThirdPartyPayer: bill.origin === 'tiers_payant',
+          subtype: bill.care_acts[0].display_label,
+          socialSecurityBase: bill.care_acts[0].ss_base / 100,
+          amount: bill.total_reimbursed_by_alan / 100,
+          originalAmount: bill.total_covered_amount / 100,
+          isThirdPartyPayer:
+            bill.total_reimbursed_by_alan != bill.total_covered_amount,
           currency: '€',
-          isRefund: true
+          isRefund: true,
+          fileAttributes: {
+            metadata: {
+              contentAuthor: 'alan.com',
+              issueDate: utils.formatDate(new Date()),
+              datetime: moment(bill.care_date, 'YYYY-MM-DD').toDate(),
+              datetimeLabel: `issueDate`,
+              isSubscription: false,
+              carbonCopy: true,
+              qualification: Qualification.getByLabel('health_invoice')
+            }
+          }
         }))
     )
   }
@@ -139,14 +178,14 @@ function linkFiles(bills, user) {
   let currentMonthIsReplaced = false
   let previousMonthIsReplaced = false
   bills = bills.map(bill => {
-    set(bill, 'fileAttributes.metadata.checkUpdate', undefined)
-    bill.fileAttributes.metadata
     bill.fileurl = `https://api.alan.eu/api/users/${
       user.userId
-    }/settlements?year=${moment(bill.date).format('YYYY')}&month=${moment(
+    }/decomptes?year=${moment(bill.date).format('YYYY')}&month=${moment(
       bill.date
     ).format('M')}`
-    bill.filename = `${moment(bill.date).format('YYYY_MM')}_alan.pdf`
+    bill.filename = `${moment(bill.date).format(
+      'YYYY_MM'
+    )}_remboursement_alan.pdf`
     const currentMonth = Number(moment().format('M'))
     const previousMonth = Number(
       moment()
